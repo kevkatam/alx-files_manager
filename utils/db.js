5aa51d425335313fee807719a920e4b1df4ad366
kevkatam <kevkatam@gmail.com>
2024-05-13 11:09:01 +0300

    adding db.js

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ class DBClient {
     const port = process.env.DB_PORT || '27017';
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
     this.client.connect().then(() => {
       this.db = this.client.db(database);
     }).catch((err) => {
@@ -19,14 +19,14 @@ class DBClient {
   }
 
   async nbUsers() {
-    const collection = this.db.collection('users');
-    const count = await collection.countDocuments();
+    const user_cols = this.db.collection('users');
+    const count = await user_cols.countDocuments();
     return count;
   }
 
   async nbFiles() {
-    const collection = this.db.collection('files');
-    const count = await collection.countDocuments();
+    const file_cols = this.db.collection('files');
+    const count = await file_cols.countDocuments();
     return count;
   }
 }
